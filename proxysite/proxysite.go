15bfb59101b73f4c584d392fcafe8a569b4b7241
daemonwhisper0101 <daemonwhisper0101@users.noreply.github.com>
2018-08-09 03:57:16 +0900

    support ProxySite

---
+++ proxysite/proxysite.go
@@ -0,0 +1,39 @@
+// vim:set sw=2 sts=2:
+package proxysite
+
+import (
+  "io/ioutil"
+  "net/http"
+  "net/http/cookiejar"
+  neturl "net/url"
+)
+
+type ProxySite struct {
+  Jar *cookiejar.Jar
+}
+
+func NewProxySite() (*ProxySite, error) {
+  jar, err := cookiejar.New(nil)
+  if err != nil {
+    return nil, err
+  }
+  return &ProxySite{ Jar: jar }, nil
+}
+
+func (ps *ProxySite)Get(url string) ([]byte, error) {
+  cl := &http.Client{ Jar: ps.Jar }
+  data := neturl.Values{}
+  data.Set("d", url)
+  data.Set("allowCookies", "on")
+  data.Set("server-option", "us1")
+  resp, err := cl.PostForm("https://us1.proxysite.com/includes/process.php?action=update", data)
+  if err != nil {
+    return nil, err
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return nil, err
+  }
+  return body, nil
+}
