de0438f4e1913e873ed531ac79325a4c065efe26
daemonwhisper0101 <daemonwhisper0101@users.noreply.github.com>
2020-05-04 09:50:51 +0000

    proxysite: GetWithReferer

--- proxysite/proxysite.go
+++ proxysite/proxysite.go
@@ -7,10 +7,12 @@ import (
   "net/http"
   "net/http/cookiejar"
   neturl "net/url"
+  "strings"
 )
 
 type ProxySite struct {
   Jar *cookiejar.Jar
+  LastURL string
   //
   Debug bool
 }
@@ -67,6 +69,51 @@ func (ps *ProxySite)Get(url string) ([]byte, error) {
   ps.dbgln(ps.Jar)
   defer resp.Body.Close()
   body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return nil, err
+  }
+  return body, nil
+}
+
+func (ps *ProxySite)GetWithReferer(url, referer string) ([]byte, error) {
+  cl := &http.Client{ Jar: ps.Jar }
+
+  // make referer url
+  data := neturl.Values{}
+  data.Set("d", referer)
+  data.Set("allowCookies", "on")
+  data.Set("server-option", "us1")
+  ps.dbgln(data)
+  resp, err := cl.PostForm("https://us1.proxysite.com/includes/process.php?action=update", data)
+  if err != nil {
+    return nil, err
+  }
+  resp.Body.Close()
+  ps.dbgln(ps.Jar)
+  ps.LastURL = resp.Request.URL.String()
+
+  // make real url
+  data = neturl.Values{}
+  data.Set("d", url)
+  data.Set("allowCookies", "on")
+  data.Set("server-option", "us1")
+  // no work req, err := http.NewRequest("POST", "https://us1.proxysite.com/includes/process.php?action=update", strings.NewReader(data.Encode()))
+  resp, err = cl.PostForm("https://us1.proxysite.com/includes/process.php?action=update", data)
+  if err != nil {
+    return nil, err
+  }
+  resp.Body.Close()
+
+  query := strings.TrimSuffix(resp.Request.URL.String(), "&f=norefer")
+  req, err := http.NewRequest("GET", query, nil)
+  req.Header.Add("Referer", ps.LastURL)
+  resp, err = cl.Do(req)
+  if err != nil {
+    return nil, err
+  }
+  ps.dbgln(resp)
+  body, err := ioutil.ReadAll(resp.Body)
+  defer resp.Body.Close()
   if err != nil {
     return nil, err
   }
